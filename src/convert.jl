b075b3c0bc3ad5c5bf7a251bf2173c1593bddfa9
Luise Prielinger <l.p.prielinger@tudelft.nl>
2026-07-14 11:28:23 +0200

    first 100 indices

--- src/convert.jl
+++ src/convert.jl
@@ -9,7 +9,7 @@ start_time = time()
 folder = length(ARGS) >= 1 ? ARGS[1] : "/Users/localadmin/Library/CloudStorage/OneDrive-DelftUniversityofTechnology/4_backup_project_piecemakerDQEC/output_v1_cluster/output_v1"
 outfolder = length(ARGS) >= 2 ? ARGS[2] : "/Users/localadmin/Library/CloudStorage/OneDrive-DelftUniversityofTechnology/4_backup_project_piecemakerDQEC/output_v1_cluster/output_v1/"
 
-for file in readdir(folder)[1:10]
+for file in readdir(folder)[1:100]
     endswith(file, ".jld2") || continue
 
     path = joinpath(folder, file)
