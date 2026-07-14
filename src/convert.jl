e57f7a0ea7a19db569c73ec89a1413aed46901c0
Luise Prielinger <l.p.prielinger@tudelft.nl>
2026-07-14 11:15:14 +0200

    added global index for parallel execution of convert

--- src/convert.jl
+++ src/convert.jl
@@ -8,35 +8,35 @@ start_time = time()
 
 folder = length(ARGS) >= 1 ? ARGS[1] : "/Users/localadmin/Library/CloudStorage/OneDrive-DelftUniversityofTechnology/4_backup_project_piecemakerDQEC/output_v1_cluster/output_v1"
 outfolder = length(ARGS) >= 2 ? ARGS[2] : "/Users/localadmin/Library/CloudStorage/OneDrive-DelftUniversityofTechnology/4_backup_project_piecemakerDQEC/output_v1_cluster/output_v1/"
+global_idx = length(ARGS) >= 3 ? parse(Int, ARGS[3]) : 1
 
+file = readdir(folder)[global_idx]
 
-for file in readdir(folder)[1:10]
-    endswith(file, ".jld2") || continue
+endswith(file, ".jld2") || error("File $(file) is not a .jld2 file.")
 
-    path = joinpath(folder, file)
-    @load path df_out
+path = joinpath(folder, file)
+@load path df_out
 
-    df_out[!, :depolarizing] = df_out[!, :error_model] .== "depolarizing"
-    df_out[!, :dephasing] = df_out[!, :error_model] .== "dephasing"
-    select!(df_out, Not(:error_model))
+df_out[!, :depolarizing] = df_out[!, :error_model] .== "depolarizing"
+df_out[!, :dephasing] = df_out[!, :error_model] .== "dephasing"
+select!(df_out, Not(:error_model))
 
-    for i in 1:4
-        df_out[!, Symbol("client_$i")] = in.(i, df_out[!, :clients_serviced])
-    end
-
-    for col in names(df_out, Int64)
-        df_out[!, col] = Int32.(df_out[!, col])
-    end
-
-    for col in names(df_out, Float64)
-        df_out[!, col] = Float32.(df_out[!, col])
-    end
-
-    select!(df_out, Not(:clients_serviced))
-
-    outpath = joinpath(outfolder, replace(file, ".jld2" => ".csv"))
-    CSV.write(outpath, df_out)
+for i in 1:4
+    df_out[!, Symbol("client_$i")] = in.(i, df_out[!, :clients_serviced])
 end
+
+for col in names(df_out, Int64)
+    df_out[!, col] = Int32.(df_out[!, col])
+end
+
+for col in names(df_out, Float64)
+    df_out[!, col] = Float32.(df_out[!, col])
+end
+
+select!(df_out, Not(:clients_serviced))
+
+outpath = joinpath(outfolder, replace(file, ".jld2" => ".csv"))
+CSV.write(outpath, df_out)
 
 end_time = time()
 @info "Conversion completed in $(end_time - start_time) seconds."
