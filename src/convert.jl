31c900272cc3756bbf26fc03962b42c274512118
Luise Prielinger <l.p.prielinger@tudelft.nl>
2026-07-14 12:31:01 +0200

    updated convert

--- src/convert.jl
+++ src/convert.jl
@@ -4,17 +4,17 @@ using DataFrames
 using CSV
 using StatsPlots
 
-start_time = time()
-
 folder = length(ARGS) >= 1 ? ARGS[1] : "/Users/localadmin/Library/CloudStorage/OneDrive-DelftUniversityofTechnology/4_backup_project_piecemakerDQEC/output_v1_cluster/output_v1"
 outfolder = length(ARGS) >= 2 ? ARGS[2] : "/Users/localadmin/Library/CloudStorage/OneDrive-DelftUniversityofTechnology/4_backup_project_piecemakerDQEC/output_v1_cluster/output_v1/"
 start_index = length(ARGS) >= 3 ? parse(Int, ARGS[3]) : 1
 
 conversion_files = readdir(folder)
-end_index = min(start_index + 100, length(conversion_files))
+end_index = min(start_index+100, length(conversion_files))
 
 
 for file in readdir(folder)[start_index:end_index]
+    start_time = time()
+
     endswith(file, ".jld2") || continue
 
     path = joinpath(folder, file)
@@ -22,8 +22,8 @@ for file in readdir(folder)[start_index:end_index]
 
     df_out[!, :depolarizing] = df_out[!, :error_model] .== "depolarizing"
     df_out[!, :dephasing] = df_out[!, :error_model] .== "dephasing"
-    select!(df_out, Not(:error_model))
 
+    select!(df_out, Not([:error_model, :time_diff, :n, :seed]))
     for i in 1:4
         df_out[!, Symbol("client_$i")] = in.(i, df_out[!, :clients_serviced])
     end
@@ -36,7 +36,7 @@ for file in readdir(folder)[start_index:end_index]
         df_out[!, col] = Float32.(df_out[!, col])
     end
 
-    select!(df_out, Not(:clients_serviced))
+    select!(df_out, Not([:clients_serviced]))
 
     outpath = joinpath(outfolder, replace(file, ".jld2" => ".csv"))
     CSV.write(outpath, df_out)
